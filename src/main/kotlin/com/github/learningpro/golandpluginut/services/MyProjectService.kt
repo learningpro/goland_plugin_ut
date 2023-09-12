607d1b75ca62334d48cb6e7f4286df2e4f066946
GitHub Action <action@github.com>
2023-09-12 13:59:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/learningpro/golandpluginut/services/MyProjectService.kt
+++ src/main/kotlin/com/github/learningpro/golandpluginut/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.learningpro.golandpluginut.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.learningpro.golandpluginut.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
